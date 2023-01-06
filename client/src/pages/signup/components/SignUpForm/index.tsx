077de4e394248689d5a29b4c3d3eb61b5aae65a7
MyeongHoonNam <codenmh0822@naver.com>
2023-01-06 23:18:13 +0900

    feat : 예외처리에 따른 UI 적용

--- client/src/pages/signup/components/SignUpForm/index.tsx
+++ client/src/pages/signup/components/SignUpForm/index.tsx
@@ -1,31 +1,57 @@
+import { useSignUpForm } from '@pages/signup/hooks';
+import { ErrorText } from '@components/index';
+
 const SignUpForm = () => {
+  const { isLoading, inputError, apiError, handleChange, handleSubmit } =
+    useSignUpForm();
+
   return (
-    <form className="w-[400px] p-[16px] shadow-md box-border border-2 [&>input:not(:first-of-type)]:mt-2 [&>button]:mt-2">
+    <form
+      onSubmit={handleSubmit}
+      className="w-[400px] p-[16px] shadow-md box-border border-2 [&>input:not(:first-of-type)]:mt-2"
+    >
       <h1 className="mb-2.5 text-2xl font-bold text-center cursor-default">
         SignUp
       </h1>
 
       <input
         type="text"
+        name="email"
+        onChange={handleChange}
         placeholder="email"
         className="block w-full py-1 px-1.5 border-2 border-[#333] selection:rounded bg-white text-sm box-border"
       />
 
+      {inputError.email && <ErrorText>{inputError.email}</ErrorText>}
+
       <input
         type="password"
+        name="password"
+        onChange={handleChange}
         placeholder="password"
         className="block w-full py-1 px-1.5 border-2 border-[#333] rounded bg-white text-sm box-border"
       />
 
+      {inputError.password && <ErrorText>{inputError.password}</ErrorText>}
+
       <input
         type="password"
+        name="passwordConfirm"
+        onChange={handleChange}
         placeholder="passwordConfirm"
         className="block w-full py-1 px-1.5 border-2 border-[#333] rounded bg-white text-sm box-border"
       />
 
+      {inputError.passwordConfirm && (
+        <ErrorText>{inputError.passwordConfirm}</ErrorText>
+      )}
+
+      {apiError && <ErrorText>{apiError.message}</ErrorText>}
+
       <button
         type="submit"
-        className="block w-full h-[32px] text-white rounded bg-black box-border cursor-pointer hover:bg-[#222] disabled:bg-[#888]"
+        disabled={isLoading}
+        className="block w-full h-[32px] mt-2 text-white rounded bg-black box-border cursor-pointer hover:bg-[#222] disabled:bg-[#888]"
       >
         SignUp
       </button>
